4d134eafec335825fc901aba9839a88a78d4abbc
sob3r69 <iamnotsob3r@gmail.com>
2025-01-31 14:25:01 +0300

    Init commit

---
+++ web/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Todos Demo</title>
+	<meta name="description" content="A sample using todos..." />
+</svelte:head>
+
+<div>
+	<h1>Todo App Example</h1>
+</div>
